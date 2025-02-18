25279aeff500fea1335dfbcf43676cbea8bc378b
Brandon Gonzalez <bgbeaconhous@gmail.com>
2025-02-18 10:10:04 -0800

    add two new files

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1. I will create a string with the user's saying.
+// 2. I will assign 3 variables. 
+// 3. Each variable will be an arithmetic operation to equal the combination.
+// 4/ I will add comments before or after each step.
+// 5. I will create a dialog box displaying the vault codes.
+
+//I have chosen 'message' to be the variable name for the opening message.
+
+let message = "You have received this message because you have chosen to open an important vault. Here is the secret combination:";
+
+//I have labeled the 3 variables as num1, num2, num3.
+//Each variable is equal to an arithmetic operation.
+
+let num1 = 11-1
+let num2 = 2 * 20
+let num3 = 33 + 6
+
+//alert message created using the 3 variables.
+//alert also includes message created on line 9.
+
+alert(`${message} the vault combo is ${num1}-${num2}-${num3}`);
+
+//Dialog box works correctly and pops up when going to browser.
